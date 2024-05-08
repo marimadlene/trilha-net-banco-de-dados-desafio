2d6a853e29fe3ff6a98c16d352e281ae960d673f
Mariel Madlene <mariel.dos.santos@hotmail.com>
2024-05-08 15:51:06 -0300

    Desafio Montando Consultas Relacionais no SQL Server

---
+++ DesafioMontandoConsultasRelacionaisSQLServer.sql
@@ -0,0 +1,43 @@
+--1
+SELECT Nome, Ano FROM Filmes
+
+--2
+SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano
+
+--3
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De volta para o futuro'
+
+--4
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano = 1997
+
+--5
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano > 2000
+
+--6
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao
+
+--7 esta com erro
+SELECT Ano, COUNT(*) Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC
+
+--8
+SELECT * FROM Atores WHERE Genero = 'M'
+
+--9
+SELECT * FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome
+
+--10
+SELECT Nome, Genero FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+
+--11
+SELECT Nome, Genero 
+FROM Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero WHERE Generos.Genero = 'Mistério'
+
+--12
+SELECT Nome, PrimeiroNome, UltimoNome, Papel 
+FROM Filmes
+INNER JOIN ElencoFilme ON ElencoFilme.IdFilme = Filmes.Id
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
